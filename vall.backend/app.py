60f292e34f88600930ed324ffba7ebfaf04357bc
Jihan Vall <jihansestvall@gmail.com>
2025-06-19 16:22:32 +0100

    updated

--- vall.backend/app.py
+++ vall.backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
+import smtplib
+from email.mime.text import MIMEText
 app=Flask(__name__)
 CORS(app)
 @app.route('/contact',methods=['POST'])
@@ -8,7 +10,21 @@ def contact():
     name=data.get('name')
     email=data.get('email')
     message=data.get('message')
-    print(f"New message from {name} ({email}): {message}")
-    return jsonify({"success":True,"message":"Message received successfully!"}),200
+    sender="hello@jihanvall.com"
+    receiver="hello@jihanvall.com"
+    password="Jihansest24."
+    msg=MIMEText(f"Message from {name} <{email}>:\n\n{message}")
+    msg['Subject']="New Conact Form Submission"
+    msg['From']=sender
+    msg['To']=receiver
+    try:
+        with smtplib.SMTP_SSL("mail.privateemail.com",465) as server:
+            server.login(sender,password)
+            server.send_message(msg)
+        return jsonify({"success":True,"message":"Message sent successfully!"}),200
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+        return jsonify({"success":False,"message":"Failed to send email."}),500    
+                      
 if __name__ =='__main__':
     app.run(debug=True)
